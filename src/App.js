032737919cfda1cbbe8bf4357c9252dadcf2ce4a
Jag <jagi.work@gmail.com>
2025-11-26 16:43:22 +0000

    new parargrah added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          new paragraph added
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
