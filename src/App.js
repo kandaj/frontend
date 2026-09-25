e99a65a49a6d3c96dae6cc4a12d81379215e72b9
Jag Kandasamy <jagi.work@gmail.com>
2026-09-25 15:56:31 +0100

    text 1 added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-       <p>new text added test</p>
+       <p>new text added test1</p>
         <a
           className="App-link"
           href="https://reactjs.org"
